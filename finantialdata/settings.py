f72720c374507013a1ea96450dddadaf2003eaeb
ankitpakhale <akp3067@gmail.com>
2022-04-25 20:43:26 +0530

    Initial Commit

--- finantialdata/settings.py
+++ finantialdata/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 APPEND_SLASH=False 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
