0d35f299384f39ecba717f722e4be172a642d043
ankitpakhale <akp3067@gmail.com>
2022-05-27 21:30:35 +0530

    Minor Changes

--- expensetrack/views.py
+++ expensetrack/views.py
@@ -170,16 +170,13 @@ def team(request):
 def testimonials(request):
     return render(request, 'testimonials.html')
 
-
 # ----------------------------------------------------------------------
 
 def about(request):
     return render(request, 'about.html')
 
-    
 def elements(request):
     return render(request, 'elements.html')
-
 
 def contact(request):
     key = ''
